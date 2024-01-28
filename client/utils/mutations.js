1052da3528c38268a5a5f98bfd288b5e66eed75c
Shoyo Kumagai <shyokmg.au@gmail.com>
2024-01-28 11:27:28 +1100

    Added utils for authentication and mutations on the client

---
+++ client/utils/mutations.js
@@ -0,0 +1,25 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
